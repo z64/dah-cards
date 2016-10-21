942a7598d958d96800ac60712b9c73837db4c1a5
Ashley Perpetual <ashleyperpetual@gmail.com>
2016-10-22 01:14:08 +0800

    Added recursive function to go through and pass all strings through (1) an html to markdown converter and (2) an html entity decoder (todo)

--- scripts/json2yaml/json2yaml.js
+++ scripts/json2yaml/json2yaml.js
@@ -8,6 +8,28 @@ var cardsJson = fs.readFileSync('cards.json');
 var cardsObj = JSON.parse(cardsJson);
 
 // for each string that could contain html in cardsObj, set the value to toMarkdown of that string. then go through and detect any &html syntax and convert that to unicode by replacing '&html' with String.fromCharCode(html) where 'html' is the value specified
+function recurse(cards) {
+  var keys = Object.keys(cards);
+
+  for (prop in keys) {
+    // keys is an array of the keys of cards, NOT the objects themselves, so prop is referring to the index OF the key of the corresponding object.
+    // NOT the key or index of the object itself!
+    var obj = cards[keys[prop]];
+    // check if the property is an object, if so we're going through again :^)
+    if (typeof obj === 'object') {
+      recurse(obj);
+    } else if (typeof obj === 'string') {
+      // run the string through a markdown converter and then find any &html codes and replace them with a unicode char using fromCharCode
+      // make sure we're modifying the cards object itself and not the temporary object!
+      cards[keys[prop]] = toMarkdown(obj);
+    }
+  }
+  return cards;
+}
+
+cardsObj = recurse(cardsObj);
+
+
 
 // do stuff to cardsObj here then save to yaml file.
 yaml.writeSync('out.yml', cardsObj);
