e617bd721b9e7ed6a7084eb14ca51a37427d0d43
Ashley Perpetual <ashleyperpetual@gmail.com>
2016-10-22 00:42:43 +0800

    Added markdown converter to package.json, wrote the pseudocode for converting html to Markdown and parsing &html chars directly into unicode characters.

--- scripts/json2yaml/json2yaml.js
+++ scripts/json2yaml/json2yaml.js
@@ -1,10 +1,13 @@
-const yaml = require('node-yaml');
-const fs = require('fs');
+var fs = require('fs');
+var yaml = require('node-yaml');
+var toMarkdown = require('to-markdown');
 
 var cardsJson = fs.readFileSync('cards.json');
 
 // parse the json string into an actual js object so we can work with it
 var cardsObj = JSON.parse(cardsJson);
+
+// for each string that could contain html in cardsObj, set the value to toMarkdown of that string. then go through and detect any &html syntax and convert that to unicode by replacing '&html' with String.fromCharCode(html) where 'html' is the value specified
 
 // do stuff to cardsObj here then save to yaml file.
 yaml.writeSync('out.yml', cardsObj);
